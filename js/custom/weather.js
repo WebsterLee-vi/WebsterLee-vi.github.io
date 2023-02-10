82743d81e94a87239f98ca0c9b4229f9ae78444f
WebsterLee <260393824@qq.com>
2023-02-10 18:11:45 +0800

    Site updated: 2023-02-10 18:11:42

---
+++ js/custom/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "FFFFFF",
+      "tmpSize": "16",
+      "cityColor": "FFFFFF",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "1",
+      "language": "auto",
+      "borderRadius": "5",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "2d96f611444d40a4b8c50d30ecfb6ada"
+    }
+  }
