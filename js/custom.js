b322eabc916de74ad0a687eb5e55e7b634d6a71e
WebsterLee <260393824@qq.com>
2023-02-10 09:07:06 +0800

    Site updated: 2023-02-10 09:07:04

--- js/custom.js
+++ js/custom.js
@@ -55,3 +55,25 @@ function browserRedirect() {
         function stopp() { if (staticx) { var child = document.getElementById("canvas_sakura"); child.parentNode.removeChild(child); window.cancelAnimationFrame(stop); staticx = false; } else { startSakura(); } }
     }
 }
+
+const cheerio = require('cheerio');
+
+/**
+ * 在页面插入新顶部图
+ * @param {cheerio.Root} $ Root
+ */
+function insertTopImg($) {
+    let header = $('#page-header');
+    if (header.length === 0) return;
+    let background = header.css('background-image');
+    if (!background) return;
+    $('#post, #page, #archive, #tag, #category').prepend(`<div class="top-img" style="background-image: ${background};"></div>`);
+}
+
+hexo.extend.filter.register('after_render:html', function(str, data) {
+    let $ = cheerio.load(str, {
+        decodeEntities: false
+    });
+    insertTopImg($);
+    return $.html();
+});
